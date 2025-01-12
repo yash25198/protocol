52998480d095b342c2a9374dea3752657b9ff277
Cliff Syner <c4syner@gmail.com>
2025-01-12 12:49:18 -0800

    chore: remove todo

--- contracts/src/libraries/Constants.sol
+++ contracts/src/libraries/Constants.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.27;
 library Constants {
     uint16 public constant DEPOSIT_LOCKUP_PERIOD = 8 hours;
     uint64 public constant MIN_OUTPUT_SATS = 1000; // to prevent dust errors
-    // TODO: Challenge period should scale with the number of blocks in the proof.
-    // Set it to 2x the estimated proof generation time for n blocks.
     uint32 public constant MIN_DEPOSIT_AMOUNT = (10e3 / PROTOCOL_FEE_BP) + 1;
     uint8 public constant PROTOCOL_FEE_BP = 5; // maker + taker fee = 0.1%
     uint8 public constant MIN_CONFIRMATION_BLOCKS = 2;
