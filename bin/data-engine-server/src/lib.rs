7b3398cfce730114ea1744388bd427716acc2061
Tristan Barrett <bruidbarrett@gmail.com>
2025-01-30 14:35:15 -0800

    CORS updates

--- bin/data-engine-server/src/lib.rs
+++ bin/data-engine-server/src/lib.rs
@@ -1,4 +1,7 @@
 use alloy::primitives::Address;
+use axum::http::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use axum::http::Method;
+use axum::response::IntoResponse;
 use axum::{extract::State, routing::get, Json, Router};
 use bitcoin_light_client_core::hasher::Digest;
 use bitcoin_light_client_core::leaves::BlockLeaf;
@@ -6,10 +9,12 @@ use clap::{command, Parser};
 use data_engine::engine::DataEngine;
 use data_engine::models::OTCSwap;
 use eyre::Result;
+use regex::Regex;
 use rift_sdk::{create_websocket_provider, DatabaseLocation};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::sync::Arc;
+use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Clone, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -33,8 +38,7 @@ pub async fn run_server(config: ServerConfig, initial_block_leaf: BlockLeaf) -> 
     .await?;
 
     if data_engine.get_leaf_count().await? == 0 {
-        // TODO: Create a more ergonomic abstraction over "seeding" the data engine if starting fresh
-        let append_result = data_engine
+        data_engine
             .indexed_mmr
             .write()
             .await
@@ -43,23 +47,36 @@ pub async fn run_server(config: ServerConfig, initial_block_leaf: BlockLeaf) -> 
         println!("Seeded data engine with genesis block leaf...");
     }
 
-    // Build the Axum router
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
+        .allow_origin(allow_rift_exchange_and_localhost());
+
     let app = Router::new()
         .route("/swaps", get(get_swaps_for_address))
         .route("/tip-proof", get(get_tip_proof))
         .route("/contract-bitcoin-tip", get(get_latest_contract_block))
         .route("/health", get(health))
+        .layer(cors)
         .with_state(Arc::new(data_engine));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], config.port));
     tracing::info!("Listening on {}", addr);
 
-    // Spawn the HTTP server as another tracked task
     axum::serve(tokio::net::TcpListener::bind(&addr).await?, app)
         .await
         .map_err(|e| eyre::eyre!(e))?;
 
     Ok(())
+}
+
+fn allow_rift_exchange_and_localhost() -> tower_http::cors::AllowOrigin {
+    tower_http::cors::AllowOrigin::predicate(|origin, _| {
+        let allowed_domains = vec!["http://localhost:3000", "http://localhost:8000"];
+        let regex = Regex::new(r"^https://.*\.rift\.exchange$").unwrap();
+        let origin_str = origin.to_str().unwrap_or("");
+        allowed_domains.contains(&origin_str) || regex.is_match(origin_str)
+    })
 }
 
 #[derive(Deserialize, Serialize)]
@@ -76,7 +93,6 @@ async fn get_swaps_for_address(
     let swaps = data_engine
         .get_virtual_swaps(query.address, query.page, None)
         .await
-        // TODO: More useful error handling
         .map_err(|e| {
             (
                 axum::http::StatusCode::BAD_REQUEST,
